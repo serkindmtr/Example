80153072a461c3e8ea8df493dfeaf66c8a510940
Dmitry Serkin <sdima96@gmail.com>
2018-03-21 15:12:48 +0300

    Update external_answer.js

--- external_answer.js
+++ external_answer.js
@@ -1,5 +1,6 @@
 {
-"mapping": [
-{"path": "https://raw.githubusercontent.com/serkindmtr/Example/master/external_answer.js", "field_id": 1101},
-]
+  "mapping": [
+              {"path": "https://raw.githubusercontent.com/serkindmtr/Example/master/external_answer.js", 
+               "field_id": 1101},
+             ]
 }
